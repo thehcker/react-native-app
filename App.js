e251b220654f5bc56727df2a42ce5c8751f2e082
ISAAC NYAKOI <isaacnyakoi1@gmail.com>
2020-05-20 08:06:22 +0300

    Add Timer react-native-app

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import EventList from "./EventList";
+
+export default function App() {
+  return (
+      <EventList />
+  );
+}
---
+++ EventForm.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { View, Text, TouchableHighlight, } from 'react-native';
+
+class EventForm extends Component {
+    render(){
+        return (
+            <View>
+                <TouchableHighlight>
+                    <Text>Add</Text>
+                </TouchableHighlight>
+            </View>
+        )
+    }
+}
+export default EventForm;
